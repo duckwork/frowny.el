6c92c976ea3d24e341162f1f1226def75baf8967
Case Duckworth <acdw@acdw.net>
2021-09-28 13:10:09 -0500

    Change license to GCL

--- frowny.el
+++ frowny.el
@@ -5,18 +5,15 @@
 ;; Author: Case Duckworth <acdw@acdw.net>
 ;; Keywords: convenience
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;;; License:
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Everyone is permitted to do whatever with this software, without
+;; limitation.  This software comes without any warranty whatsoever,
+;; but with two pieces of advice:
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; - Be kind to yourself.
+
+;; - Make good choices.
 
 ;;; Commentary:
 
